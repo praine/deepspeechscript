4c57646587337a2b93ebd69de643d08cc32be66e
justinhunt <bitwalkerjapan@gmail.com>
2020-12-10 23:48:15 +0900

    added compand to transcode

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ function convertAndTranscribe(audiofile, scorerfile){
 
     var proc = ffmpeg(audiofile)
         .format('wav')
-	.audioFilters('afftdn')
+	    .audioFilters(['afftdn','compand=0|0:1|1:-90/-900|-70/-70|-30/-9|0/-3:6:0:0:0'])
         .audioCodec('pcm_s16le')
         .audioBitrate(16)
         .audioChannels(1)
