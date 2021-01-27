29cacd617f2e0a34c381aaea1acf682b21fcb067
Ubuntu <ubuntu@ip-172-31-25-172.ap-northeast-1.compute.internal>
2021-01-27 04:31:43 +0000

    worker_threads and queue

--- server.js
+++ server.js
@@ -2,41 +2,54 @@
     Standard imports
  **************************************************/
 const express = require('express');
-const queue = require('express-queue');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const _ = require('lodash');
 const app = express();
-const DeepSpeech = require('deepspeech');
 const request = require("request");
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const fileUpload = require("express-fileupload");
 const nodefetch = require('node-fetch');
-
-const ACTIVE_LIMIT = 1;
-const QUEUED_LIMIT = 100;
-const STD_MODEL = "/home/ubuntu/deepspeech-0.9.3-models.tflite"
-const STD_SCORER = "/home/ubuntu/deepspeech-0.9.3-models.scorer"
-const STD_SAMPLE_RATE = 16000;
-
-function createModel(modelPath, scorerPath) {
-  let model = new DeepSpeech.Model(modelPath);
-  model.enableExternalScorer(scorerPath);
-  return model;
-}
-
-function metadataToString(all_metadata, idx) {
-  var transcript = all_metadata.transcripts[idx];
-  var retval = ""
-  for (var i = 0; i < transcript.tokens.length; ++i) {
-    retval += transcript.tokens[i].text;
+const queue = require('express-queue');
+const queueMw = queue({
+  activeLimit: 1,
+  queuedLimit: 5,
+  rejectHandler: (req, res) => {
+    console.log("queue limit reached: rejecting request..");
+    var id;
+    if (req.body.hasOwnProperty("id")) {
+      id = req.body.id;
+    } else {
+      id = null;
+    }
+    res.send({
+      id: id,
+      result: "error",
+      message: 'Server busy'
+    });
   }
-  return retval;
-}
+});
 
+const {
+  Worker
+} = require('worker_threads');
+
+function runService(workerData) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(__dirname + '/deepspeech.js', {
+      workerData
+    });
+    worker.on('message', resolve);
+    worker.on('error', reject);
+    worker.on('exit', (code) => {
+      if (code !== 0)
+        reject(new Error(`Worker stopped with exit code ${code}`));
+    })
+  })
+}
 app.use(fileUpload({
   createParentPath: true
 }));
@@ -47,24 +60,6 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(morgan('dev'));
-const queueMw = queue({
-  activeLimit: ACTIVE_LIMIT,
-  queuedLimit: QUEUED_LIMIT,
-  rejectHandler: (req, res) => {
-    var id;
-    if (req.body.hasOwnProperty("id")) {
-      id = req.body.id;
-    } else {
-      id = null;
-    }
-    return res.send({
-      id: id,
-      result: "error",
-      message: 'Server busy. Please try again.'
-    });
-  }
-});
-
 app.use(queueMw);
 
 app.get("/", (req, res) => {
@@ -73,87 +68,47 @@ app.get("/", (req, res) => {
 
 });
 
-app.post('/stt', (req, res) => {
+app.post('/stt', async function(req, res) {
 
-  writeLog(`queueLength: ${queueMw.queue.getLength()}`);
-
+  var id;
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-  try {
+  writeLog("/stt: endpoint was triggered", ip, req.body.origin);
 
-    var id;
+  if (req.body.hasOwnProperty("id")) {
+    id = req.body.id;
+  } else {
+    id = null;
+  }
 
-    if (req.body.hasOwnProperty("id")) {
-      id = req.body.id;
-    } else {
-      id = null;
-    }
-
-    if (!req.files) {
-      return res.send({
-        id: id,
-        result: "error",
-        message: 'No blob specified'
-      });
-    }
-
-    if (!req.body.origin) {
-      return res.send({
-        id: id,
-        result: "error",
-        message: 'No origin specified'
-      });
-    }
-
-    writeLog("/stt: endpoint triggered", ip, req.body.origin);
-
-    var tmpname = Math.random().toString(20).substr(2, 6);
-
-    if (req.body.scorer) {
-      var b64scorer = req.body.scorer;
-      var buf = Buffer.from(b64scorer, 'base64');
-      fs.writeFileSync("/home/ubuntu/uploads/" + tmpname + "_scorer", buf);
-    }
-
-    fs.writeFileSync("/home/ubuntu/uploads/" + tmpname + "_blob", req.files.blob.data);
-
-    var model, beamWidth;
-
-    if (req.body.scorer) {
-      beamWidth = 500;
-      model = createModel(STD_MODEL, "/home/ubuntu/uploads/" + tmpname + "_scorer");
-    } else {
-      beamWidth = 2000;
-      model = createModel(STD_MODEL, STD_SCORER);
-    }
-
-    model.setBeamWidth(beamWidth);
-
-    var maxAlternates = 1;
-    var audioBuffer = fs.readFileSync("/home/ubuntu/uploads/" + tmpname + "_blob");
-
-    var result = model.sttWithMetadata(audioBuffer, maxAlternates);
-
-    if (req.body.scorer) {
-      deleteFile("/home/ubuntu/uploads/" + tmpname + "_scorer");
-    }
-
-    deleteFile("/home/ubuntu/uploads/" + tmpname + "_blob");
-
-    var transcript = metadataToString(result, 0);
-
-    writeLog("/stt: got result (" + transcript + ")", ip, req.body.origin);
-
+  if (!req.files) {
     return res.send({
       id: id,
-      result: "success",
-      transcript: transcript,
+      result: "error",
+      message: 'No blob specified'
     });
-
-
-  } catch (err) {
-    writeLog("/stt: error (" + JSON.stringify(err) + ")", ip, req.body.origin);
   }
+
+  if (!req.body.origin) {
+    return res.send({
+      id: id,
+      result: "error",
+      message: 'No origin specified'
+    });
+  }
+
+  var result = await runService({
+    blob: req.files.blob.data,
+    scorer: req.body.scorer
+  })
+
+  writeLog("/stt: got result (" + result.transcript + ")", ip, req.body.origin);
+
+  return res.send({
+    id: id,
+    result: "success",
+    transcript: result.transcript
+  });
 
 });
 
@@ -163,17 +118,8 @@ var server = http.createServer(app);
 
 server.listen(port, () => console.log(`App is listening on port ${port}.`));
 
-/* HELPER FUNCTIONS */
-
-function deleteFile(path) {
-  try {
-    fs.unlinkSync(path);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
 function writeLog(log, ip, origin) {
+  console.log(log);
   nodefetch('http://ec2-18-183-39-101.ap-northeast-1.compute.amazonaws.com:3000/write_log', {
       method: 'post',
       body: JSON.stringify({
